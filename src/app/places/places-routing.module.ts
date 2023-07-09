a07858158d8958536d8ad8b7effcd31db944c12e
goncalo-alexandre02 <goncalo.alexandre@vr2work.com>
2023-07-09 17:08:29 +0100

    creation of folders and pages

---
+++ src/app/places/places-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { PlacesPage } from './places.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PlacesPage
+  },
+  {
+    path: 'discover',
+    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+  },
+  {
+    path: 'offers',
+    loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+  },
+  {
+    path: 'place-detail',
+    loadChildren: () => import('./discover/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PlacesPageRoutingModule {}
